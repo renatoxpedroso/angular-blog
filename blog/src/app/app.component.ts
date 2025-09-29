bac12ff6f04cc4a3540fdc8642addee0c19f90e1
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2025-09-29 19:29:19 -0300

    menu title

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SmallCardComponent],
+  imports: [RouterOutlet, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
