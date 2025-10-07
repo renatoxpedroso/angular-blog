90ce0424756a2d48a1d17dcb1e086e3d523ca5a4
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2025-10-07 19:40:19 -0300

    Criação de cards

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuTitleComponent],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
